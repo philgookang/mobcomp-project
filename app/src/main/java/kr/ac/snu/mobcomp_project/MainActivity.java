266711664edf6b6cca95976af67f86f8005f70b4
Oh Hyun Seok <ohsai@snu.ac.kr>
2019-04-28 16:29:55 +0900

    Designated_call
    dd

--- app/src/main/java/kr/ac/snu/mobcomp_project/MainActivity.java
+++ app/src/main/java/kr/ac/snu/mobcomp_project/MainActivity.java
@@ -1,13 +1,56 @@
 package kr.ac.snu.mobcomp_project;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ViewPager vp;
+    public String designated_phone_number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vp = (ViewPager)findViewById(R.id.vp);
+        vp.setAdapter(new pagerAdapter( getSupportFragmentManager()));
+        vp.setCurrentItem(0);
+
+        // load settings
+        loadSetting();
+        }
+    private void loadSetting(){
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String defaultValue = getResources().getString(R.string.designated_phone_number_default);
+        designated_phone_number = sharedPref.getString(getString(R.string.designated_phone_number), defaultValue);
     }
+
+    private class pagerAdapter extends FragmentStatePagerAdapter
+    {
+        public pagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position) {
+                case 0:
+                    return new TabFragment1();
+                case 1:
+                    return new TabFragment2();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+
 }
---
+++ app/src/main/java/kr/ac/snu/mobcomp_project/TabFragment1.java
@@ -0,0 +1,139 @@
+package kr.ac.snu.mobcomp_project;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class TabFragment1 extends Fragment
+{
+    private Button button;
+    private final int CALL_PHONE_PERMISSIONS = 1;
+    private int prev_state;
+    public TabFragment1()
+    {
+
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ConstraintLayout layout = (ConstraintLayout) inflater.inflate(R.layout.tab_fragment_1, container, false);
+        button = layout.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                checkPermissionandCall(arg0);
+            }
+        });
+        TelephonyManager mTelephonyManager = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+        PhoneStateListener mPhoneStateListener = new PhoneStateListener(){
+            private static final String TAG = "mPhoneStateListener";
+            @Override
+            public void onCallStateChanged(int state, String phoneNumber) {
+                //super.onCallStateChanged(state, phoneNumber);
+                switch(state){
+                    case TelephonyManager.CALL_STATE_RINGING :
+                        Log.d(TAG,"CALL_STATE_RINGING");
+                        prev_state = TelephonyManager.CALL_STATE_RINGING;
+                        break;
+                    case TelephonyManager.CALL_STATE_IDLE :
+                        Log.d(TAG,"CALL_STATE_IDLE");
+                        if((prev_state == TelephonyManager.CALL_STATE_RINGING)){
+                            Log.d(TAG,"REJECTED_OR_MISSED_CALL");
+                        }
+                        else if((prev_state == TelephonyManager.CALL_STATE_OFFHOOK)){
+                            Log.d(TAG,"ANSWERED_CALL");
+                        }
+                        prev_state = TelephonyManager.CALL_STATE_IDLE;
+                        break;
+                    case TelephonyManager.CALL_STATE_OFFHOOK :
+                        Log.d(TAG,"CALL_STATE_OFFHOOK");
+                        prev_state = TelephonyManager.CALL_STATE_OFFHOOK;
+                        break;
+
+                }
+            }
+        };
+        mTelephonyManager.listen(mPhoneStateListener,PhoneStateListener.LISTEN_CALL_STATE);
+        return layout;
+    }
+    private void checkPermissionandCall(View arg0){
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.CALL_PHONE)) {
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE},
+                        CALL_PHONE_PERMISSIONS);
+            } else {
+
+                // No explanation needed, we can request the permission.
+                requestPermissions( new String[]{Manifest.permission.CALL_PHONE},
+                        CALL_PHONE_PERMISSIONS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            return;
+        }
+        else {
+            start_call();
+        }
+    }
+    private void start_call() {
+        String phone_no = "tel:" +((MainActivity)getActivity()).designated_phone_number;
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse(phone_no));
+
+        startActivity(callIntent);
+        //mTelephonyManager.listen(mPhoneStateListener,PhoneStateListener.LISTEN_NONE);
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case CALL_PHONE_PERMISSIONS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    start_call();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+}
