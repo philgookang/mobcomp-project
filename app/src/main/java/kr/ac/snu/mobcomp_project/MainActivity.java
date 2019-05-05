92c2fe1ba2960f418716835550334f5387c84584
Do Il Yoon <ttotto3080@gmail.com>
2019-05-05 12:52:09 +0900

    implement get current time

--- app/src/main/java/kr/ac/snu/mobcomp_project/MainActivity.java
+++ app/src/main/java/kr/ac/snu/mobcomp_project/MainActivity.java
@@ -2,6 +2,11 @@ package kr.ac.snu.mobcomp_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getTime(View view) {
+        TextView currTime = findViewById(R.id.date);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        String currDateTime = sdf.format(new Date());
+        currTime.setText(currDateTime);
     }
 }
