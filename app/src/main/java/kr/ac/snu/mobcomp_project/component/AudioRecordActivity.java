f2ec58b99ea43da7467d6a0e0e87c90f121e135a
creamchoco3080 <ttotto3080@gmail.com>
2019-05-07 18:06:52 +0900

    implement speech recognition

--- app/src/main/java/kr/ac/snu/mobcomp_project/component/AudioRecordActivity.java
+++ app/src/main/java/kr/ac/snu/mobcomp_project/component/AudioRecordActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -16,10 +17,11 @@ import android.view.View;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import kr.ac.snu.mobcomp_project.R;
 
-public class AudioRecordActivity extends AppCompatActivity {
+public class AudioRecordActivity extends AppCompatActivity implements RecognitionListener {
 
     SpeechRecognizer sr = null;
 
@@ -38,6 +40,7 @@ public class AudioRecordActivity extends AppCompatActivity {
         } else {
             if(SpeechRecognizer.isRecognitionAvailable(this)) {
                 sr = SpeechRecognizer.createSpeechRecognizer(this);
+                sr.setRecognitionListener(this);
             }
         }
     }
@@ -53,7 +56,9 @@ public class AudioRecordActivity extends AppCompatActivity {
     public void startRecording(View view) {
         System.out.println("startRecording");
         if(sr != null) {
-            sr.startListening(new Intent());
+            Intent srIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+            sr.startListening(srIntent);
         }
     }
 
@@ -62,6 +67,42 @@ public class AudioRecordActivity extends AppCompatActivity {
         if(sr != null) {
             sr.stopListening();
         }
+    }
+
+    private void log1(String message){Log.d("moo", message);}
+    private void log2(String message){Log.d("cow", message);}
+
+    @Override public void onReadyForSpeech(Bundle params){log1("onReadyForSpeech");}
+    @Override public void onBeginningOfSpeech(){log1("onBeginningOfSpeech");}
+    @Override public void onRmsChanged(float rms_dB){log2("onRmsChanged");}
+    @Override public void onBufferReceived(byte[] buffer){log1("onBufferReceived");}
+    @Override public void onEndOfSpeech(){log1("onEndOfSpeech");}
+    @Override public void onResults(Bundle results){
+        log1("onResults");
+        ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        float[] scores = results.getFloatArray(SpeechRecognizer.CONFIDENCE_SCORES);
+        for(int i = 0; i < scores.length; i++) {
+            log1(matches.get(i));
+            log1(String.valueOf(scores[i]));
+        }
+    }
+    @Override public void onPartialResults(Bundle partialResults){log1("onPartialResults");}
+    @Override public void onEvent(int eventType, Bundle params){log1("onEvent");}
+    @Override public void onError(int error)
+    {
+        String message = "";
+
+        if(error == SpeechRecognizer.ERROR_AUDIO)                           message = "audio";
+        else if(error == SpeechRecognizer.ERROR_CLIENT)                     message = "client";
+        else if(error == SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS)   message = "insufficient permissions";
+        else if(error == SpeechRecognizer.ERROR_NETWORK)                    message = "network";
+        else if(error == SpeechRecognizer.ERROR_NETWORK_TIMEOUT)            message = "network timeout";
+        else if(error == SpeechRecognizer.ERROR_NO_MATCH)                   message = "no match found";
+        else if(error == SpeechRecognizer.ERROR_RECOGNIZER_BUSY)            message = "recognizer busy";
+        else if(error == SpeechRecognizer.ERROR_SERVER)                     message = "server";
+        else if(error == SpeechRecognizer.ERROR_SPEECH_TIMEOUT)             message = "speech timeout";
+
+        log1("error " + message);
     }
 
 }
