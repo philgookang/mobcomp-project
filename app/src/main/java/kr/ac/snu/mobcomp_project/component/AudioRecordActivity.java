ba2075c93e156b4a6929f195e069112ad3bb326a
creamchoco3080 <ttotto3080@gmail.com>
2019-05-07 17:10:43 +0900

    start implementing SpeechRecognizer

--- app/src/main/java/kr/ac/snu/mobcomp_project/component/AudioRecordActivity.java
+++ app/src/main/java/kr/ac/snu/mobcomp_project/component/AudioRecordActivity.java
@@ -1,10 +1,12 @@
 package kr.ac.snu.mobcomp_project.component;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.os.Environment;
+import android.speech.RecognitionListener;
+import android.speech.SpeechRecognizer;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -19,7 +21,7 @@ import kr.ac.snu.mobcomp_project.R;
 
 public class AudioRecordActivity extends AppCompatActivity {
 
-    MediaRecorder recorder;
+    SpeechRecognizer sr = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,49 +31,36 @@ public class AudioRecordActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        File directory = new File(getFilesDir()+"");
-        File[] files = directory.listFiles();
-        for(int i = 0; i < files.length; i++) {
-            Log.d("Files", "FileName:" + files[i].getName());
-        }
-
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.RECORD_AUDIO}, 0);
+        } else {
+            if(SpeechRecognizer.isRecognitionAvailable(this)) {
+                sr = SpeechRecognizer.createSpeechRecognizer(this);
+            }
         }
     }
 
     @Override
     protected void onDestroy() {
-        if(recorder != null) {
-            recorder.reset();
-            recorder.release();
-            recorder = null;
+        if(sr != null) {
+            sr.destroy();
         }
         super.onDestroy();
     }
 
     public void startRecording(View view) {
         System.out.println("startRecording");
-        recorder = new MediaRecorder();
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        String fileDir = getFilesDir() + "/currAudio.3gp";
-        recorder.setOutputFile(fileDir);
-        try {
-            recorder.prepare();
-            recorder.start();
-        } catch (IOException exp) {
-            System.err.println(exp);
+        if(sr != null) {
+            sr.startListening(new Intent());
         }
     }
 
     public void stopRecording(View view) {
         System.out.println("stopRecording");
-        if(recorder != null) {
-            recorder.stop();
+        if(sr != null) {
+            sr.stopListening();
         }
     }
 
