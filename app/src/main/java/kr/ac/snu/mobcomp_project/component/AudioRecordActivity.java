b9c99bc2d68cd107cf07a3b0116901105060e7df
creamchoco3080 <ttotto3080@gmail.com>
2019-05-07 14:25:45 +0900

    clean up record start/stop

--- app/src/main/java/kr/ac/snu/mobcomp_project/component/AudioRecordActivity.java
+++ app/src/main/java/kr/ac/snu/mobcomp_project/component/AudioRecordActivity.java
@@ -4,12 +4,15 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import java.io.File;
 import java.io.IOException;
 
 import kr.ac.snu.mobcomp_project.R;
@@ -26,42 +29,42 @@ public class AudioRecordActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        File directory = new File(getFilesDir()+"");
+        File[] files = directory.listFiles();
+        for(int i = 0; i < files.length; i++) {
+            Log.d("Files", "FileName:" + files[i].getName());
+        }
+
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.RECORD_AUDIO}, 0);
-        } else {
-            this.setUpRecorder();
-            System.out.println(getFilesDir());
         }
     }
 
-    private void setUpRecorder() {
-        if(recorder == null) {
-            recorder = new MediaRecorder();
-            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-            String fileDir = getFilesDir() + "";
-            System.out.println(fileDir);
-            recorder.setOutputFile(fileDir);
-        } else {
-            System.out.println("Recorder not null");
+    @Override
+    protected void onDestroy() {
+        if(recorder != null) {
+            recorder.reset();
             recorder.release();
             recorder = null;
         }
+        super.onDestroy();
     }
 
     public void startRecording(View view) {
         System.out.println("startRecording");
-        if(recorder != null) {
-            try {
-                recorder.prepare();
-                recorder.start();
-            } catch (IOException exp) {
-                System.err.println(exp);
-            }
-        } else {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        String fileDir = getFilesDir() + "/currAudio.3gp";
+        recorder.setOutputFile(fileDir);
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (IOException exp) {
+            System.err.println(exp);
         }
     }
 
@@ -69,9 +72,6 @@ public class AudioRecordActivity extends AppCompatActivity {
         System.out.println("stopRecording");
         if(recorder != null) {
             recorder.stop();
-            recorder.reset();
-            recorder.release();
-            recorder = null;
         }
     }
 
