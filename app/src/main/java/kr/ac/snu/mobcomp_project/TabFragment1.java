5be94c7cbe3588ff9a3dd1be5ae2661d7a5be4a6
Hyunik Kim <hyunik.kim@snu.ac.kr>
2019-06-09 01:09:03 +0900

    Camera Preview Success

--- app/src/main/java/kr/ac/snu/mobcomp_project/TabFragment1.java
+++ app/src/main/java/kr/ac/snu/mobcomp_project/TabFragment1.java
@@ -2,10 +2,7 @@ package kr.ac.snu.mobcomp_project;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -17,7 +14,6 @@ import android.support.v4.content.ContextCompat;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -33,24 +29,21 @@ import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import kr.ac.snu.mobcomp_project.afterdetection.CallManager;
 import kr.ac.snu.mobcomp_project.component.AccelerometerListener;
-import kr.ac.snu.mobcomp_project.component.AudioRecordActivity;
+import kr.ac.snu.mobcomp_project.component.CameraSourcePreview;
 import kr.ac.snu.mobcomp_project.component.EyeTracker;
 import kr.ac.snu.mobcomp_project.component.LocationMonitor;
-import kr.ac.snu.mobcomp_project.example.EyeTrackerExampleActivity;
 
-public class TabFragment1 extends Fragment implements SurfaceHolder.Callback
-{
+public class TabFragment1 extends Fragment implements SurfaceHolder.Callback {
     //Calling
     private Button button;
     private final int CALL_PHONE_PERMISSIONS = 1;
     private int prev_state;
     private CallManager mCallManager;
 
+    private CameraSourcePreview mPreview;
 
     //View
     ConstraintLayout layout;
@@ -60,114 +53,116 @@ public class TabFragment1 extends Fragment implements SurfaceHolder.Callback
     private Handler mHandler;
     Runnable mRunnable;
 
-    public TabFragment1()
-    {
+    public TabFragment1() {
 
     }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //load components
-        ((MainActivity)getActivity()).mAccelerometerListener = new AccelerometerListener(this);
-        ((MainActivity)getActivity()).mLocationMonitor = new LocationMonitor(getActivity(), savedInstanceState,this); // Why
+        ((MainActivity) getActivity()).mAccelerometerListener = new AccelerometerListener(this);
+        ((MainActivity) getActivity()).mLocationMonitor = new LocationMonitor(getActivity(), savedInstanceState, this); // Why
 
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if(((MainActivity)getActivity()).mCameraSource != null)
-            ((MainActivity)getActivity()).mCameraSource.release();
+        if (((MainActivity) getActivity()).mCameraSource != null)
+            ((MainActivity) getActivity()).mCameraSource.release();
     }
 
-    public void updateValue(float acceleration, float[] gravity){
+    public void updateValue(float acceleration, float[] gravity) {
         TextView txtacc = (TextView) layout.findViewById(R.id.acceleration);
         TextView txtgravity = (TextView) layout.findViewById(R.id.gravity);
-        if(txtacc != null) {
+        if (txtacc != null) {
             txtacc.setText(String.format("Acceleration | %.6f", acceleration));
         }
-        if(txtgravity != null) {
-            txtgravity.setText(String.format("Gravity | %.6f %.6f %.6f", gravity[0],gravity[1],gravity[2]));
+        if (txtgravity != null) {
+            txtgravity.setText(String.format("Gravity | %.6f %.6f %.6f", gravity[0], gravity[1], gravity[2]));
         }
     }
-    public void updateLocationValue(double latitude, double longitude, float speed){
+
+    public void updateLocationValue(double latitude, double longitude, float speed) {
         TextView txtgps = (TextView) layout.findViewById(R.id.gps_location);
-        if(txtgps != null){
+        if (txtgps != null) {
             System.out.println(latitude);
-            Log.d("Fragment1", "latitude: " + String.valueOf(latitude) + "   longitude: " + String.valueOf(longitude) + "   speed: " + String.valueOf(speed) );
+            Log.d("Fragment1", "latitude: " + String.valueOf(latitude) + "   longitude: " + String.valueOf(longitude) + "   speed: " + String.valueOf(speed));
             txtgps.setText(String.format("GPS | %.6f , %.6f , %.6f", latitude, longitude, speed));
             //txtgps.setText(String.format("Original Value a b c d e f g", (float)latitude));
-        }
-        else{
+        } else {
             System.out.println("Cannot find txtgps");
         }
     }
-    public void updateDLInference(float[] lableProbArray, int length){
+
+    public void updateDLInference(float[] lableProbArray, int length) {
         TextView txtinf = (TextView) layout.findViewById(R.id.Inference);
-        if(txtinf != null){
+        if (txtinf != null) {
             String temp = "";
-            for(int i = 0; i < length; i++) {
-                temp = String.format("%s%.2f, ",temp,lableProbArray[i]);
+            for (int i = 0; i < length; i++) {
+                temp = String.format("%s%.2f, ", temp, lableProbArray[i]);
             }
-            txtinf.setText(String.format("DL | %s ",temp));
-        }
-        else{
+            txtinf.setText(String.format("DL | %s ", temp));
+        } else {
             System.out.println("Cannot find txtinf");
         }
     }
-    public void updateMLInference(int output){
+
+    public void updateMLInference(int output) {
         TextView txtinf = (TextView) layout.findViewById(R.id.svm);
-        if(txtinf != null){
-            txtinf.setText(String.format("ML | %d ",output));
-        }
-        else{
+        if (txtinf != null) {
+            txtinf.setText(String.format("ML | %d ", output));
+        } else {
             System.out.println("Cannot find txtsvm");
         }
     }
 
     @Override
     public void onResume() {
-        ((MainActivity)getActivity()).mAccelerometerListener.onThreadResume();
-        ((MainActivity)getActivity()).mLocationMonitor.onResume();
+        ((MainActivity) getActivity()).mAccelerometerListener.onThreadResume();
+        ((MainActivity) getActivity()).mLocationMonitor.onResume();
         //getTime();
 
         // load inference task
         mHandler = new Handler();
-        mRunnable = new DrowsyDetector(getActivity(),this,mHandler,((MainActivity)getActivity()).mAccelerometerListener,((MainActivity) getActivity()).mLocationMonitor);
+        mRunnable = new DrowsyDetector(getActivity(), this, mHandler, ((MainActivity) getActivity()).mAccelerometerListener, ((MainActivity) getActivity()).mLocationMonitor);
         mRunnable.run();
         ((MainActivity) getActivity()).mRunnable = mRunnable;
-        try {
+/*        try {
             if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                ((MainActivity)getActivity()).mCameraSource.start(surfaceView.getHolder());
+                //((MainActivity) getActivity()).mCameraSource.start(surfaceView.getHolder());
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
         super.onResume();
-
+        startCameraSource();
     }
 
     @Override
     public void onPause() {
-        ((MainActivity)getActivity()).mAccelerometerListener.onThreadPause();
-        ((MainActivity)getActivity()).mLocationMonitor.onPause();
+        ((MainActivity) getActivity()).mAccelerometerListener.onThreadPause();
+        ((MainActivity) getActivity()).mLocationMonitor.onPause();
         // remove inference task
-        ((DrowsyDetector)mRunnable).close();
+        ((DrowsyDetector) mRunnable).close();
         mHandler.removeCallbacks(mRunnable);
         ((MainActivity) getActivity()).mRunnable = null;
-        if (((MainActivity)getActivity()).mCameraSource != null){
-            ((MainActivity)getActivity()).mCameraSource.stop();
+        if (((MainActivity) getActivity()).mCameraSource != null) {
+            ((MainActivity) getActivity()).mCameraSource.stop();
         }
         super.onPause();
-
+        mPreview.stop();
     }
+
     private Button labelbutton;
     public boolean drowsiness;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        layout = (ConstraintLayout)inflater.inflate(R.layout.tab_fragment_1, container, false);
-
+        layout = (ConstraintLayout) inflater.inflate(R.layout.tab_fragment_1, container, false);
+        mPreview = (CameraSourcePreview)layout.findViewById(R.id.camerapreview);
         // Designated Call managing part
         mCallManager = new CallManager();
         button = layout.findViewById(R.id.button);
@@ -183,9 +178,9 @@ public class TabFragment1 extends Fragment implements SurfaceHolder.Callback
         labelbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(drowsiness){
+                if (drowsiness) {
                     labelbutton.setText("Awake");
-                }else{
+                } else {
                     labelbutton.setText("Drowsy");
                 }
                 drowsiness = !drowsiness;
@@ -202,20 +197,22 @@ public class TabFragment1 extends Fragment implements SurfaceHolder.Callback
 
         return layout;
     }
+
     public int frontcam = 0;
 
     private final int CAMERA_PERMISSIONS = 9;
-    public boolean requestPermissionCamera(){
-        if ( ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ) {
+
+    public boolean requestPermissionCamera() {
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(getActivity(), "Need Camera Permission", Toast.LENGTH_LONG).show();
-            ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.CAMERA
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA
             }, CAMERA_PERMISSIONS);
-        }
-        else{
+        } else {
             createCameraSource();
         }
         return true;
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) { // Permisson check callback
@@ -223,18 +220,18 @@ public class TabFragment1 extends Fragment implements SurfaceHolder.Callback
             case CALL_PHONE_PERMISSIONS: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) { // Permission Granted
-                    if(mCallManager != null){
+                    if (mCallManager != null) {
                         mCallManager.start_call(this);
                     }
                 } else {
                 }
                 return;
             }
-            case CAMERA_PERMISSIONS : {
+            case CAMERA_PERMISSIONS: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) { // Permission Granted
                     createCameraSource();
-                }else{
+                } else {
 
                 }
             }
@@ -244,7 +241,7 @@ public class TabFragment1 extends Fragment implements SurfaceHolder.Callback
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
-        ((MainActivity)getActivity()).mLocationMonitor.onSaveInstanceState(outState);
+        ((MainActivity) getActivity()).mLocationMonitor.onSaveInstanceState(outState);
         super.onSaveInstanceState(outState);
     }
     /*public void getTime() {
@@ -256,6 +253,7 @@ public class TabFragment1 extends Fragment implements SurfaceHolder.Callback
 
     private static final String TAG = "EyeTracker";
     EyeTracker mEyeTracker;
+
     private class EyeTrackerFactory implements MultiProcessor.Factory<Face> {
 
         @Override
@@ -275,7 +273,7 @@ public class TabFragment1 extends Fragment implements SurfaceHolder.Callback
         detector.setProcessor(
                 new MultiProcessor.Builder<>(new EyeTrackerFactory()).build());
 
-        ((MainActivity)getActivity()).mCameraSource = new CameraSource.Builder(context, detector)
+        ((MainActivity) getActivity()).mCameraSource = new CameraSource.Builder(context, detector)
                 .setRequestedPreviewSize(360, 360)
                 .setFacing(CameraSource.CAMERA_FACING_FRONT)
                 .setRequestedFps(30.0f)
@@ -284,26 +282,27 @@ public class TabFragment1 extends Fragment implements SurfaceHolder.Callback
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        surfaceView = (SurfaceView)layout.findViewById(R.id.camerapreview);
-        surfaceHolder = surfaceView.getHolder();
-        surfaceHolder.addCallback(this);
+       // surfaceView = (SurfaceView) layout.findViewById(R.id.camerapreview);
+     //   surfaceHolder = surfaceView.getHolder();
+     //   surfaceHolder.addCallback(this);
     }
 
 
     SurfaceView surfaceView;
     SurfaceHolder surfaceHolder;
+
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-            try {
-                if (ActivityCompat.checkSelfPermission((MainActivity)getActivity(),
-                        Manifest.permission.CAMERA)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    ((MainActivity)getActivity()).mCameraSource.start(holder);
-                    Log.d("SURFACE","Created");
-                }
-            }catch (IOException e){
-                e.printStackTrace();
+        try {
+            if (ActivityCompat.checkSelfPermission((MainActivity) getActivity(),
+                    Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED) {
+                ((MainActivity) getActivity()).mCameraSource.start(holder);
+                Log.d("SURFACE", "Created");
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -351,7 +350,20 @@ public class TabFragment1 extends Fragment implements SurfaceHolder.Callback
             previewing = false;
         }
         */
-        Log.d("SURFACE","Destroyed");
-        ((MainActivity)getActivity()).mCameraSource.stop();
+        Log.d("SURFACE", "Destroyed");
+        ((MainActivity) getActivity()).mCameraSource.stop();
+    }
+
+    private void startCameraSource() {
+
+        if (((MainActivity) getActivity()).mCameraSource != null) {
+            try {
+                mPreview.start(((MainActivity) getActivity()).mCameraSource);
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to start camera source.", e);
+                ((MainActivity) getActivity()).mCameraSource.release();
+                ((MainActivity) getActivity()).mCameraSource = null;
+            }
+        }
     }
 }
